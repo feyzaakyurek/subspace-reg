80d796d660429b8ad81f465ee2d1cfddd601075b
feyzaakyurek <feyzatoksal@gmail.com>
2020-09-03 18:03:25 -0400

    forward opt too in creating model

--- models/util.py
+++ models/util.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from . import model_dict
 
 
-def create_model(name, n_cls, dataset='miniImageNet', vocab=None):
+def create_model(name, n_cls, opt, vocab=None, dataset='miniImageNet'):
     """create model by name"""
     if dataset == 'miniImageNet' or dataset == 'tieredImageNet':
         if name.endswith('v2') or name.endswith('v3'):
@@ -13,7 +13,9 @@ def create_model(name, n_cls, dataset='miniImageNet', vocab=None):
             print('use imagenet-style resnet50')
             model = model_dict[name](num_classes=n_cls)
         elif name.startswith('resnet') or name.startswith('seresnet'):
-            model = model_dict[name](avg_pool=True, drop_rate=0.1, dropblock_size=5, num_classes=n_cls, vocab=vocab)
+            model = model_dict[name](avg_pool=True, drop_rate=0.1, 
+                                     dropblock_size=5, num_classes=n_cls, 
+                                     vocab=vocab, opt=opt) #TODO
         elif name.startswith('wrn'):
             model = model_dict[name](num_classes=n_cls)
         elif name.startswith('convnet'):
@@ -43,3 +45,4 @@ def get_teacher_name(model_path):
             return segments[0]
         else:
             return segments[0] + '_' + segments[1] + '_' + segments[2]
+        
