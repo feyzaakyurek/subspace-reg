bb1188b09527569c2d7bc277ccdda306ac27c509
feyzaakyurek <feyzatoksal@gmail.com>
2020-09-03 18:07:03 -0400

    alternative commands

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,12 +3,14 @@
 # ======================
 
 # supervised pre-training
-python train_supervised.py --trial pretrain --model_path dumped --tb_path tb --data_root data  
-#python train_supervised.py --trial pretrain --model_path dumped/  --data_root data/
+# python train_supervised.py --trial pretrain --model_path dumped --tb_path tb --data_root data  
 
 # distillation
 # setting '-a 1.0' should give simimlar performance
 #python train_distillation.py -r 0.5 -a 0.5 --path_t /path/to/teacher.pth --trial born1 --model_path /path/to/save --tb_path /path/to/tensorboard --data_root /path/to/data_root
 
 # evaluation
-#python eval_fewshot.py --model_path /path/to/student.pth --data_root /path/to/data_root
+# python eval_fewshot.py --model_path /path/to/student.pth --data_root /path/to/data_root
+
+# supervised pretraining with class labels
+python train_supervised.py --trial pretrain --model_path dumped --tb_path tb --data_root data --classifier lang-linear --word_embed_size 500
--- scripts/run_inc_eval.sh
+++ scripts/run_inc_eval.sh
@@ -1,1 +1,8 @@
-python eval_incremental.py --model_path dumped/resnet12_last.pth --data_root data --n_shots 5 --incremental_eval
+# Zero shot evaluation with biased lang classifier
+# python eval_incremental.py --model_path dumped/resnet12_last_langclassifier_wbias.pth --data_root data --n_shots 5 --eval_mode zero-shot-novel-only --word_embed_size 500 --lang_classifier_bias
+
+# Zero shot evaluation with non-biased lang classifier
+# python eval_incremental.py --model_path dumped/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain_exp0/resnet12_lastFalse.pth --data_root data --n_shots 5 --eval_mode zero-shot-novel-only --word_embed_size 500
+
+# Incremental evaluation with non-biased lang classifier
+python eval_incremental.py --model_path dumped/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/resnet12_lastFalse.pth --data_root data --n_shots 5 --eval_mode zero-shot-incremental --classifier lang-linear --word_embed_size 500 --start_alpha 0.12 --end_alpha 0.25 --inc_alpha 0.02 --num_novel_combs 10
