9c510a298c64ebe31fa376a075eec2662a99d684
feyzaakyurek <feyzatoksal@gmail.com>
2020-10-12 15:21:07 -0400

    backbone linear classifier no bias training script

--- scripts/slurm_run_backbone.sh
+++ scripts/slurm_run_backbone.sh
@@ -5,41 +5,48 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:volta:1
-#SBATCH --array=1-3
+#SBATCH --array=1
 #SBATCH --output=dumped/%A_%a.out
 #SBATCH --error=dumped/%A_%a.err
-#SBATCH --job-name=lil-backbone
+#SBATCH --job-name=lil-backbone-no-bias
 
 
 
 # Create the combinations of params for each array task,
 # and save them to a temp params file.
-DUMPED_PATH="/home/gridsan/akyurek/git/rfs-incremental/dumped/backbones/label"
-FILE="$DUMPED_PATH/${SLURM_ARRAY_TASK_ID}_temp_hyperparameters.txt"
-rm $FILE 
+DUMPED_PATH="/home/gridsan/akyurek/git/rfs-incremental/dumped/backbones/linear"
+# FILE="$DUMPED_PATH/${SLURM_ARRAY_TASK_ID}_temp_hyperparameters.txt"
+# rm $FILE 
 
 
-for MULTIPFC in 0.1 0.2 0.3; do
-    echo "${MULTIPFC}" >> $FILE
-done
+# for MULTIPFC in 0.1 0.2 0.3; do
+#     echo "${MULTIPFC}" >> $FILE
+# done
 
 
 
 # Read the SLURM_ARRAY_TASK_ID line from the params file.
-LINE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $FILE) 
-read -ra PARAMS<<< "$LINE"
+# LINE=$(sed "${SLURM_ARRAY_TASK_ID}q;d" $FILE) 
+# read -ra PARAMS<<< "$LINE"
 
-MULTIPFC="${PARAMS[0]}"
+# MULTIPFC="${PARAMS[0]}"
 
 
 # Create log files
 LOG_STDOUT="${DUMPED_PATH}/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out"
 LOG_STDERR="${DUMPED_PATH}/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.err"
 
+# python train_supervised.py --trial pretrain \
+#                            --model_path dumped/backbones/label  \
+#                            --data_root data \
+#                            --multip_fc $MULTIPFC \
+#                            --classifier lang-linear \
+#                            --word_embed_size 500 > $LOG_STDOUT 2> $LOG_STDERR
+
 python train_supervised.py --trial pretrain \
-                           --model_path dumped/backbones/label  \
-                           --data_root data \
-                           --multip_fc $MULTIPFC \
-                           --classifier lang-linear \
-                           --word_embed_size 500 > $LOG_STDOUT 2> $LOG_STDERR
+                            --model_path $DUMPED_PATH \
+                            --tb_path tb \
+                            --data_root data \
+                            --classifier linear \
+                            --no_linear_bias > $LOG_STDOUT 2> $LOG_STDERR
                            
