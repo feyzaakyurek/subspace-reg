893816d2cd9e4859d39d2fd3846e75dc2f51f30d
ekinakyurek <akyurekekin@gmail.com>
2020-12-10 18:20:35 -0500

    scripts

--- scripts/slurm_run_backbone.sh
+++ scripts/slurm_run_backbone.sh
@@ -5,44 +5,47 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:volta:1
-#SBATCH --array=1-9
+#SBATCH --array=1-16
 #SBATCH --output=%A_%a.out
 #SBATCH --error=s%A_%a.err
-#SBATCH --job-name=att_reg_back
+#SBATCH --job-name=attreg_back_query_sizes
 
 # Create the combinations of params for each array task,
 # and save them to a temp params file.
-export DUMPED_PATH="/home/gridsan/eakyurek/gitother/rfs-incremental/dumped/backbone_finetune_random"
-export DATA_PATH="/home/gridsan/eakyurek/akyureklab_shared/rfs-incremental/data"
-export WORD_EMBEDS="/home/gridsan/eakyurek/akyureklab_shared/rfs-incremental/word_embeds"
+DUMPED_PATH="/home/gridsan/eakyurek/gitother/rfs-incremental/dumped/backbones/c-x-concat/size_experiments"
+DATA_PATH="/home/gridsan/eakyurek/akyureklab_shared/rfs-incremental/data"
+WORD_EMBEDS="/home/gridsan/eakyurek/akyureklab_shared/rfs-incremental/word_embeds"
 
-# FILE="$DUMPED_PATH/${SLURM_ARRAY_TASK_ID}_temp_hyperparameters.txt"
-# rm $FILE
 
 cnt=0
 for MULTIPFC in 0.075 0.01; do
-    for DIAG_REG in 0.05 0.075; do
-        (( cnt++ ))
-        if [[ $cnt -eq $SLURM_ARRAY_TASK_ID ]]; then
-            echo "${MULTIPFC} ${DIAG_REG}"
-            # Create log files
-            export LOG_STDOUT="${DUMPED_PATH}/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out"
-#             export LOG_STDERR="${DUMPED_PATH}/${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.err"
-
-            python -u train_supervised.py --trial pretrain \
-                                       --model_path $DUMPED_PATH  \
-                                       --data_root data \
-                                       --multip_fc $MULTIPFC \
-                                       --data_root $DATA_PATH \
-                                       --classifier lang-linear \
-                                       --attention concat \
-                                       --diag_reg $DIAG_REG \
-                                       --word_embed_type ".random" \
-                                       --word_embed_path $WORD_EMBEDS &> $LOG_STDOUT # 2> $LOG_STDERR
-            break
-        fi
-    done
+for DIAG_REG in 0.05 0.075; do
+for QUERY_SIZE in 300 400 500 750; do
+(( cnt++ ))
+if [[ $cnt -eq $SLURM_ARRAY_TASK_ID ]]; then
+#     echo "${MULTIPFC} ${DIAG_REG}"
+    EXP_NAME=attention_concat_multipfc_${MULTIPFC}_diagreg_${DIAG_REG}_querysize_${QUERY_SIZE}
+    EXP_FOLDER=$DUMPED_PATH/$EXP_NAME
+    mkdir -p $EXP_FOLDER
+    LOG_STDOUT="${DUMPED_PATH}/${EXP_NAME}_${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out"
+    python -u train_supervised.py --trial pretrain \
+                                  --model_path $EXP_FOLDER  \
+                                  --data_root data \
+                                  --multip_fc $MULTIPFC \
+                                  --data_root $DATA_PATH \
+                                  --classifier lang-linear \
+                                  --attention concat \
+                                  --transform_query_size $QUERY_SIZE \
+                                  --diag_reg $DIAG_REG \
+                                  --word_embed_path $WORD_EMBEDS &> $LOG_STDOUT
+#     break
+fi
 done
+done
+done
+
+# --word_embed_type ".random" \
+
 
 #
 #
