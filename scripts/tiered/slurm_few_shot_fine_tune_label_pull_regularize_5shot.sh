bfdd44aa1e1abc78b8a4b48e12e2c0313117c27a
feyzaakyurek <feyzatoksal@gmail.com>
2021-04-13 12:19:00 -0400

    small fix

--- scripts/tiered/slurm_few_shot_fine_tune_label_pull_regularize_5shot.sh
+++ scripts/tiered/slurm_few_shot_fine_tune_label_pull_regularize_5shot.sh
@@ -5,25 +5,26 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:volta:1
-#SBATCH --array=1-8
+#SBATCH --array=1-24
 #SBATCH --output=dumped/%A_%a.out
 #SBATCH --error=dumped/%A_%a.err
-#SBATCH --job-name=tiered_random5
+#SBATCH --job-name=tiered_pull5
 
 
 DUMPED_PATH="/home/gridsan/akyurek/git/rfs-incremental/dumped"
 EXP_FOLDER=$DUMPED_PATH/"tiered/finetune_5shot_label_pull"
 DATA_PATH="/home/gridsan/akyurek/git/rfs-incremental/data"
-BACKBONE_PATH="${DUMPED_PATH}/backbones/tieredImageNet/linear/resnet18/bias_false/resnet12_last.pth"
+BACKBONE_PATH="${DUMPED_PATH}/backbones/tieredImageNet/linear/resnet18/bias_false/resnet18_last.pth"
 mkdir -p $EXP_FOLDER
 
 cnt=0
 for LMBD in 0.2 0.3; do
 for TRLOSS in 1.0 1.2; do
-for PULL in 0.2 0.3; do
+for PULL in 0.05 0.2 0.3; do
+for LR in 0.002 0.001; do
 (( cnt++ ))
 if [[ $cnt -eq $SLURM_ARRAY_TASK_ID ]]; then
-    EXP_NAME=random_lambda_${LMBD}_trloss_${TRLOSS}_pull_${PULL}_${SLURM_ARRAY_TASK_ID}
+    EXP_NAME=lambda_${LMBD}_trloss_${TRLOSS}_pull_${PULL}_lr_${LR}_${SLURM_ARRAY_TASK_ID}
     LOG_STDOUT="${EXP_FOLDER}/${EXP_NAME}.out"
     LOG_STDERR="${EXP_FOLDER}/${EXP_NAME}.err"
     python eval_incremental.py --model_path $BACKBONE_PATH \
@@ -33,8 +34,8 @@ if [[ $cnt -eq $SLURM_ARRAY_TASK_ID ]]; then
                                --eval_mode few-shot-incremental-fine-tune \
                                --classifier linear \
                                --novel_epochs 10 \
-                               --learning_rate 0.001 \
-                               --word_embed_path "random_embeds" \
+                               --model resnet18 \
+                               --learning_rate $LR \
                                --freeze_backbone_at 1 \
                                --label_pull $PULL \
                                --glove \
@@ -45,7 +46,7 @@ fi
 done
 done
 done
-
+done
 # For debugging. 
 
 # No language fine tuning few-shot with label pull
@@ -62,7 +63,7 @@ done
 #                                --pulling regularize \
 #                                --lmbd_reg_transform_w 2.0 \
 #                                --target_train_loss 2.0
-
+# --use_episodes \
 
 # # No language fine tuning few-shot
 # export DUMPED_PATH="dumped"
